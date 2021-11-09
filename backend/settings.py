bb200b07a9cbafe5b8876400ebaa2216ab030b73
Trevor Garrett <trevor.garrett@psinc.net>
2021-11-08 16:02:14 -0800

    fix photo upload path to aws

--- backend/settings.py
+++ backend/settings.py
@@ -150,9 +150,12 @@ STATICFILES_DIRS = [BASE_DIR / 'static', BASE_DIR / 'frontend/build/static']
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 CORS_ALLOW_ALL_ORIGINS = True
 AWS_QUERYSTRING_AUTH = 'false'
-AWS_S3_SIGNATURE_VERSION = 's3'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_CUSTOM_DOMAIN = 'instaimgur-bucket.s3.amazonaws.com'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
